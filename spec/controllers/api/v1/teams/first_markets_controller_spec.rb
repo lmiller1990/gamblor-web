11096fd2870a06e4934c8e0ce585d565bf874cf9
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-09-11 23:52:40 +0900

    Fix tests

--- spec/controllers/api/v1/teams/first_markets_controller_spec.rb
+++ spec/controllers/api/v1/teams/first_markets_controller_spec.rb
@@ -12,7 +12,7 @@ describe Api::V1::Teams::FirstMarketsController, type: :controller do
     it 'returns array of data representing if team got first market' do
       get :show, params: { team_id: team.id, id: 'first_blood' }
 
-      expect(json_response['data'].length).to eq 2
+      expect(json_response.length).to eq 2
     end
   end
 end
