b206d84ad20469edcefa04e1255040607bb3fe5c
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-10-05 17:02:03 +0900

    Serve static assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  config.serve_static_assets = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
