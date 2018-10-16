b029e75fb1062aa0ae238d60efc055d0576636e5
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-10-16 15:13:44 +0900

    Show odds for markets in incomplete games

--- app/javascript/test/current_matchup/match_history_row.spec.js
+++ app/javascript/test/current_matchup/match_history_row.spec.js
@@ -4,9 +4,9 @@ import MatchHistoryRow from '../../src/current_matchup/match_history_row.vue'
 const context = describe
 
 describe('MatchHistoryRow', () => {
-  const factory = ({ victory, gameCompleted = true }) => 
+  const factory = ({ victory = undefined, gameCompleted = true, odds = 0.0 }) => 
     shallowMount(MatchHistoryRow, {
-      propsData: { victory, gameCompleted }
+      propsData: { victory, gameCompleted, odds }
     })
 
   context('game is completed', () => {
@@ -31,11 +31,11 @@ describe('MatchHistoryRow', () => {
 
   context('game has not been played', () => {
     it('does not render marker or give victory/defeat class', () => {
-      const wrapper = factory({ victory: undefined, gameCompleted: false })
+      const wrapper = factory({ gameCompleted: false, odds: 1.5 })
 
       expect(wrapper.classes()).not.toContain('defeat')
       expect(wrapper.classes()).not.toContain('victory')
-      expect(wrapper.text()).toBe('')
+      expect(wrapper.text()).toBe('1.5')
     })
   })
 })
