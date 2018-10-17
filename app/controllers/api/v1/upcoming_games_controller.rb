e5d68308802c8ff5a010afe83e845eb0d5b142ce
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-10-17 23:49:13 +0900

    Remove default split

--- app/controllers/api/v1/upcoming_games_controller.rb
+++ app/controllers/api/v1/upcoming_games_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class UpcomingGamesController < ActionController::API
       def index
-        split_id = upcoming_games_params[:split_id] || default_split.id
+        split_id = upcoming_games_params[:split_id]
         games = Split.find(split_id).games
 
         render json: Schedule.most_recently_played(games, 10) + Schedule.upcoming(games, 6)
@@ -12,10 +12,6 @@ module Api
 
       def upcoming_games_params
         params.permit(:split_id)
-      end
-
-      def default_split
-        Split.find_by_name(EasySettings['default_split'])
       end
     end
   end
--- spec/controllers/api/v1/upcoming_games_controller_spec.rb
+++ spec/controllers/api/v1/upcoming_games_controller_spec.rb
@@ -21,14 +21,5 @@ describe Api::V1::UpcomingGamesController, type: :controller do
         expect(json_response.last['id']).to eq game_6_days_from_now.id
       end
     end
-
-    context 'split_id is not specified' do
-      it 'gets past 10 and upcoming 6 games using default split' do
-        get :index
-
-        expect(json_response.first['id']).to eq game_10_days_ago.id
-        expect(json_response.last['id']).to eq game_6_days_from_now.id
-      end
-    end
   end
 end
