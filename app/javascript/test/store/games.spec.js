25e78755c57fca4f7f2305e687d76d565dd6e56e
Lachlan <lachlan.miller.1990@outlook.com>
2018-10-24 15:25:20 +0900

    Calculate EV correctly using most recent games (#33)
    * Fix bug where EV is calc. using first N games instead of last N games
    
    * Fix bug where EV is NaN for all games
    
    * Update test desc.

--- app/javascript/test/store/games.spec.js
+++ app/javascript/test/store/games.spec.js
@@ -1,6 +1,8 @@
 import { Game } from '../../src/types/game'
 import { state, mutations, getters } from '../../src/store/games.ts'
 
+const context = describe
+
 const GAME_ID = 1
 const BLUE_TEAM_ID = 1
 const RED_TEAM_ID = 2
@@ -9,15 +11,16 @@ const teamIds = { blueSideTeamId: BLUE_TEAM_ID, redSideTeamId: RED_TEAM_ID }
 const winLoseIds = { winnerId: BLUE_TEAM_ID, loserId: RED_TEAM_ID }
 
 const games = [
-  { ...winLoseIds, ...teamIds, firstBloodTeamId: BLUE_TEAM_ID, 
-    blueSideTeamFbOdds: ODDS, id: GAME_ID },
-  { ...winLoseIds, ...teamIds, firstBloodTeamId: BLUE_TEAM_ID },
-  { ...winLoseIds, ...teamIds, firstBloodTeamId: BLUE_TEAM_ID },
-  { ...winLoseIds, ...teamIds, firstBloodTeamId: RED_TEAM_ID },
+  { id: 0, ...winLoseIds, ...teamIds, firstBloodTeamId: BLUE_TEAM_ID },
+  { id: GAME_ID, ...winLoseIds, ...teamIds, firstBloodTeamId: BLUE_TEAM_ID, 
+    blueSideTeamFbOdds: ODDS },
+  { id: 2, ...winLoseIds, ...teamIds, firstBloodTeamId: BLUE_TEAM_ID },
+  { id: 3, ...winLoseIds, ...teamIds, firstBloodTeamId: BLUE_TEAM_ID },
+  { id: 4, ...winLoseIds, ...teamIds, firstBloodTeamId: RED_TEAM_ID },
   // since the last game lacks win/lose ids, it is the only 'incomplete' game
   // we do not want to use unplayed games when calculating EV.
   // therefore, this last game is not included in ev calculation.
-  { ...teamIds, firstBloodTeamId: BLUE_TEAM_ID  }
+  { id: 5, ...teamIds, firstBloodTeamId: BLUE_TEAM_ID  }
 ]
 
 const createState = () => Object.assign({}, state)
@@ -46,7 +49,7 @@ describe('getters', () => {
   describe('oddsForMarket', () => {
     it('returns in game by team and market', () => {
       const state = createState()
-      state.all = { [GAME_ID]: games[0] }
+      state.all = { [GAME_ID]: games[1] }
       state.ids = [GAME_ID]
 
       const actual = getters.oddsForMarket(state)({
@@ -59,19 +62,31 @@ describe('getters', () => {
   })
 
   describe('evByTeamId', () => {
-    it('returns ev based on percent * odds formula', () => {
-      const state = createState()
-
-      const actual = getters.evByTeamId(state, getters, {}, rootGetters)({
+    const subject = ({ nLastGames }) =>
+      getters.evByTeamId({}, getters, {}, rootGetters)({
         teamId: BLUE_TEAM_ID,
         opponentId: RED_TEAM_ID,
         market: 'fb',
         odds: ODDS,
-        nLastGames: games.length
+        nLastGames
       })
 
-      // (.75 chance to get + .75 opp. chance to not get / 2) * 2 odds
-      expect(actual).toBe(1.5)
+    context('requesting ev for nLastGames', () => {
+      it('returns ev based on percent * odds formula', () => {
+        const actual = subject({ nLastGames: 4 })
+        // (.75 chance to get + .75 opp. chance to not get / 2) * 2 odds
+        expect(actual).toBe(1.5)
+      })
+    })
+
+    context('requesting ev all past games', () => {
+      it('returns ev based on percent * odds formula', () => {
+        const actual = subject({ nLastGames: -1 })
+        // (.75 chance to get + .75 opp. chance to not get / 2) * 2 odds
+        // ((4/5) + (4/5) / 2) * 2
+        // .8 + .8 = 1.6
+        expect(actual).toBe(1.6)
+      })
     })
   })
 })
