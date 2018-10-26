32d227e3a8b6cd6cf991de7bb1c6a5cb4bdd4647
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-10-27 00:41:37 +0900

    Force SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
 
   config.serve_static_assets = true
 
+  config.force_ssl = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
