402925b3d3f36d00fe69dba8d2028b71ba9945a3
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-10-31 17:38:16 +0900

    Add kelly criterion

---
+++ app/javascript/src/bankroll/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Full Kelly Criterion
+ * Percent. to bet = Overlay / (odds - 1)
+ * Good reference here: https://www.bettingexpert.com/academy/betting-fundamentals/managing-your-bankroll
+ */
+export function fullKelly(bankroll: number, ev: number, odds: number): number {
+  const overlay = calcOverlay(ev)
+  return Math.ceil(bankroll * (overlay / (odds - 1)))
+}
+
+export function fractionalKelly(bankroll: number, ev: number, odds: number, fraction: number): number {
+  return fullKelly(bankroll, ev, odds) * fraction
+}
+
+/**
+ * overlay is the same as EV
+ * but normally presented as a percentage
+ */
+export function calcOverlay(ev: number): number {
+  return parseFloat((ev - 1).toFixed(2))
+}
