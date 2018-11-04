a31e3de046286ceb16dd294bf83084d286d74724
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-11-05 01:25:19 +0900

    Style component

--- app/javascript/src/components/sidebar_controls.vue
+++ app/javascript/src/components/sidebar_controls.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <button @click="show('Schedule')">Schedule</button>
-    <button @click="show('BetSleeve')">Bet Sleeve</button>
-    <button @click="show('HowToUse')">How To Use</button>
-    <SignOutForm />
+  <div class="controls_wrapper">
+    <div class="buttons">
+      <button @click="show('Schedule')" data-test-schedule>
+        Schedule
+      </button>
+      <button @click="show('BetSleeve')">Bet Sleeve</button>
+      <button @click="show('HowToUse')">How To Use</button>
+    </div>
+
+    <div class="signout_wrapper">
+      <div class="signout">
+        <SignOutForm />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,3 +34,29 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style scoped>
+button {
+  border: 1px solid grey;
+  width: 70px;
+  margin: 2px;
+}
+
+.controls_wrapper {
+  margin: 10px 36px;
+}
+
+.buttons {
+  display: flex;
+  justify-content: space-between;
+}
+
+.signout_wrapper {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.signout {
+  display: inline-block;
+}
+</style>
---
+++ app/javascript/test/components/sidebar_controls.spec.js
@@ -0,0 +1,18 @@
+import { shallowMount } from '@vue/test-utils'
+import SidebarControls from '../../src/components/sidebar_controls.vue'
+
+describe('SidebarControls', () => {
+  it('renders', () => {
+    const wrapper = shallowMount(SidebarControls)
+  })
+
+  describe('show', () => {
+    it('emits an event based on the arg when btn is clicked', () => {
+      const wrapper = shallowMount(SidebarControls)
+
+      wrapper.find('[data-test-schedule]').trigger('click')
+
+      expect(wrapper.emitted().showSchedule).toBeDefined()
+    })
+  })
+})
