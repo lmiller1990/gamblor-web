2b6cf266be1554df626405854137a1e2fb8204e2
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-12-25 19:06:37 +1000

    Add team logos

--- scripts/clone_production_db.sh
+++ scripts/clone_production_db.sh
@@ -1,4 +1,4 @@
-rm latest.dump &&
+[ ! -e file ] || rm latest.dump &&
 heroku pg:backups:capture &&
 heroku pg:backups:download &&
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d web_development latest.dump
