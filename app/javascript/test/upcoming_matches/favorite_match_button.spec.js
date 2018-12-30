1bad15694516797cd79d3b7acf76bd87896c373b
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-12-30 18:37:17 +1000

    Update path for import

--- app/javascript/test/upcoming_matches/favorite_match_button.spec.js
+++ app/javascript/test/upcoming_matches/favorite_match_button.spec.js
@@ -1,6 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 
-import FavoriteMatchButton from '../upcoming_matches/favorite_match_button.vue'
+import FavoriteMatchButton from '../../src/upcoming_matches/favorite_match_button.vue'
 
 describe('FavoriteMatchButton', () => {
   it('renders', () => {
