e8b015c39fa7cae9947299b0df61d01798a13051
lmiller1990 <lachlan.miller.1990@outlook.com>
2019-01-06 19:21:25 +1000

    Update deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 cd frontend
 yarn build
 cd ..
-cat public/dist/index.html app/views/app/index.html.erb.template >> app/views/app/index.html.erb
+cat public/dist/index.html app/views/app/index.html.erb.template > app/views/app/index.html.erb
