c110cca62a175cb46ce23fc3978558f44ccb824e
Lachlan Miller <lachlan@LachlansMacBook.lan>
2019-02-17 16:18:27 +1000

    Add some util methods to bet

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -5,4 +5,14 @@ class Bet < ApplicationRecord
   belongs_to :user
 
   validates :market, presence: true
+
+  def teams
+    game = Game.find(game_id) 
+
+    "#{game.blue_side_team.name} vs #{game.red_side_team.name}"
+  end
+
+  def team_bet_on
+    Team.find(team_bet_on_id).name
+  end
 end
